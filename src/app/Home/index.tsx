80ccc281c689844b976144fb9cb407803d1d47fc
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-05-30 12:13:23 -0300

    add assests and create components `Button`

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from "react-native";
+import { Image, View } from "react-native";
 
 import { styles } from "./styles";
+
+import { Button } from "@/components/Button";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
+      <Image style={styles.logo} source={require("@/assets/logo.png")} />
+      <Button title="Adicionar" />
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -3,13 +3,13 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#d0d2d8",
     alignItems: "center",
+    paddingTop: 62,
+    padding: 24,
   },
-  text: {
-    fontSize: 32,
-    fontWeight: 700,
-    color: "red",
+  logo: {
+    height: 34,
+    width: 134,
   },
 });
